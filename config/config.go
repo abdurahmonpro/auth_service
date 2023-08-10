2f72959b218baa1f88792e435b815d2be5e99e0e
abdurahmon-saydullayev <abdurahmonsaydullayev5@gmail.com>
2023-08-10 15:32:01 +0500

    auth_service is done

---
+++ config/config.go
@@ -0,0 +1,75 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/cast"
+)
+
+const (
+	// DebugMode indicates service mode is debug.
+	DebugMode = "debug"
+	// TestMode indicates service mode is test.
+	TestMode = "test"
+	// ReleaseMode indicates service mode is release.
+	ReleaseMode = "release"
+)
+
+type Config struct {
+	ServiceName string
+	Environment string // debug, test, release
+	Version     string
+
+	PostgresHost     string
+	PostgresPort     int
+	PostgresUser     string
+	PostgresPassword string
+	PostgresDatabase string
+
+	SecretKey string
+
+	AuthServiceHost string
+	AuthGRPCPort    string
+
+	PostgresMaxConnections int32
+}
+
+// Load ...
+func Load() Config {
+	if err := godotenv.Load("/ibron_go_user_service.env"); err != nil {
+		fmt.Println("No .env file found")
+	}
+
+	config := Config{}
+
+	config.ServiceName = cast.ToString(getOrReturnDefaultValue("SERVICE_NAME", "auth_service"))
+	config.Environment = cast.ToString(getOrReturnDefaultValue("ENVIRONMENT", DebugMode))
+	config.Version = cast.ToString(getOrReturnDefaultValue("VERSION", "1.0"))
+
+	config.SecretKey = "hello"
+
+	config.PostgresHost = cast.ToString(getOrReturnDefaultValue("POSTGRES_HOST", "localhost"))
+	config.PostgresPort = cast.ToInt(getOrReturnDefaultValue("POSTGRES_PORT", 5432))
+	config.PostgresUser = cast.ToString(getOrReturnDefaultValue("POSTGRES_USER", "abdurahmon"))
+	config.PostgresPassword = cast.ToString(getOrReturnDefaultValue("POSTGRES_PASSWORD", "aus1003"))
+	config.PostgresDatabase = cast.ToString(getOrReturnDefaultValue("POSTGRES_DATABASE", "auth_service"))
+
+	config.AuthServiceHost = cast.ToString(getOrReturnDefaultValue("AUTH_SERVICE_HOST", "localhost"))
+	config.AuthGRPCPort = cast.ToString(getOrReturnDefaultValue("AUTH_GRPC_PORT", ":9102"))
+
+	config.PostgresMaxConnections = cast.ToInt32(getOrReturnDefaultValue("POSTGRES_MAX_CONNECTIONS", 30))
+
+	return config
+}
+
+func getOrReturnDefaultValue(key string, defaultValue interface{}) interface{} {
+	val, exists := os.LookupEnv(key)
+
+	if exists {
+		return val
+	}
+
+	return defaultValue
+}
